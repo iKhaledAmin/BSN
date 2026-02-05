6a7f6e79abc6f0d62b4b093b02ba5e9fe5e15f80
Khaled Amin <mohamedkhaid394@gmail.com>
2026-02-05 17:15:35 +0200

    ✨ Create spring boot project and add the main configuration

---
+++ book-social-network/src/main/java/com/Khaled_Amin/book_social_network/BookSocialNetworkApplication.java
@@ -0,0 +1,15 @@
+package com.Khaled_Amin.book_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BookSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkApplication.class, args);
+	}
+
+}
